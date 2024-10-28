d681385d8b74e77e46befb4777fd57a5a08f7fe4
harrison <d.harrison.lima@gmail.com>
2024-10-28 13:30:01 -0300

    feat: add todo features, router pages and todo page

--- src/app.js
+++ src/app.js
@@ -1,58 +1,25 @@
 import React from "react";
-import * as Components from './components';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import { TodoWrapper } from "./features/todo/TodoWrapper";
 
 function App() {
-    const [signIn, toggle] = React.useState(true);
-     return(
-         <Components.Container>
-             <Components.SignUpContainer signinIn={signIn}>
-                 <Components.Form>
-                     <Components.Title>Cadraste-se</Components.Title>
-                     <Components.Input type='text' placeholder='Nome' />
-                     <Components.Input type='email' placeholder='Email' />
-                     <Components.Input type='Senha' placeholder='Senha' />
-                     <Components.Button>Cadastrar</Components.Button>
-                 </Components.Form>
-             </Components.SignUpContainer>
-
-             <Components.SignInContainer signinIn={signIn}>
-                  <Components.Form>
-                      <Components.Title>Login</Components.Title>
-                      <Components.Input type='email' placeholder='Email' />
-                      <Components.Input type='Senha' placeholder='Senha' />
-                      <Components.Anchor href='#'>Esqueceu a senha?</Components.Anchor>
-                      <Components.Button>Entrar</Components.Button>
-                  </Components.Form>
-             </Components.SignInContainer>
-
-             <Components.OverlayContainer signinIn={signIn}>
-                 <Components.Overlay signinIn={signIn}>
-
-                 <Components.LeftOverlayPanel signinIn={signIn}>
-                     <Components.Title>Bem-vindo de volta!</Components.Title>
-                     <Components.Paragraph>
-                         Para se conectar conosco faça login com as suas informações pessoais! 
-                     </Components.Paragraph>
-                     <Components.GhostButton onClick={() => toggle(true)}>
-                         Login
-                     </Components.GhostButton>
-                     </Components.LeftOverlayPanel>
-
-                     <Components.RightOverlayPanel signinIn={signIn}>
-                       <Components.Title>Bem-vindo!</Components.Title>
-                       <Components.Paragraph>
-                         Insira seus dados pessoais e comece a sua jornada conosco!
-                       </Components.Paragraph>
-                           <Components.GhostButton onClick={() => toggle(false)}>
-                               Cadraste-se
-                           </Components.GhostButton> 
-                     </Components.RightOverlayPanel>
- 
-                 </Components.Overlay>
-             </Components.OverlayContainer>
-
-         </Components.Container>
-     )
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/home" element={<TodoWrapper />} />
+          <Route path="/" element={<Navigate to="/auth" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
